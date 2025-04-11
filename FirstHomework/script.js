13f542c08821dbf4b7b444101810b8a99b97900c
pixel <biien.deniis@gmail.com>
2025-04-11 09:06:14 +0300

    finished 5 task

---
+++ FirstHomework/script.js
@@ -0,0 +1,37 @@
+// Завдання 2
+// console.log("Biien")
+
+//Завдання 3
+
+// let year = 2025;
+// let month = "April";
+// console.log(year, month)
+// let year = "April";
+// let month = "April";
+// console.log(year, month)
+
+// Завдання 4
+
+// let line = String;
+// let number = Number;
+// let logic = Boolean;
+// let unknown = undefined;
+// let zero = null;
+
+// Завдання 5
+
+// let isAdult = confirm("Are you an adult?");
+// console.log(isAdult)
+
+// Завдання 6 
+let firstName, lastName, working_group, birthday;
+firstName = "John"; //string
+lastName = "Doe"; //string
+working_group = "Engineering"; //string
+birthday = "1990-05-15"; //string
+let isMarried = true //boolean
+console.log(isMarried, firstName, lastName, working_group, birthday)
+let emptyness, unknown;
+emptyness = null;
+unknown = undefined;
+console.log(emptyness,unknown)
