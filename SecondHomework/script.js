3f340691d3b94752db0ebbcaa8498c8aee90c781
pixel <biien.deniis@gmail.com>
2025-04-22 22:30:28 +0300

    finished 4 exircises from practice 2

---
+++ SecondHomework/script.js
@@ -0,0 +1,74 @@
+// Вправа 1.1
+// let x = 1;
+// let y = 2;
+
+// let res1 = String(x) + String(y);
+// console.log(res1); // "12"
+// console.log(typeof res1); // "string"
+
+// Вправа 1.2
+// let x = 1;
+// let y = 2;
+
+// let res2 = Boolean(x) + String(y);
+// console.log(res2); // "true2"
+// console.log(typeof res2); // "string"
+
+// Вправа 1.3
+// let x = 1;
+// let y = 2;
+
+// let res3 = Boolean(x) || Boolean(y);
+// console.log(res3); // true
+// console.log(typeof res3); // "boolean"
+
+// Вправа 1.4
+// let x = 1;
+// let y = 2;
+
+// let res4 = Number("x + y");
+// console.log(res4); // NaN
+// console.log(typeof res4); // "number"
+
+// Вправа 2
+ 
+// let numCheck = prompt('Введіть своє число: ', ''); //create message window with empty bar 
+
+// if (numCheck === null) { //checking if user cancels input
+//     alert('До зустрічі!');
+// }
+//   else if (isNaN(numCheck)) { //checking if input is not a number
+//     console.log('Ви не ввели число');
+
+// } else {
+//     numCheck = +numCheck;
+//     if (!Number.isInteger(numCheck)) {
+//         console.log('Ваше число не є цілим');
+//     } else if (numCheck !== 0 && numCheck % 7 === 0) { // Check if number is non-zero and divisible by 7
+//         console.log('Ваше число кратне 7');
+//     } else if (numCheck > 0 && numCheck % 2 === 0) { // Check if number is positive and even
+//         console.log('Ваше число парне і додатне');
+//     } else { // Handle all other cases
+//         console.log('Ваше число не підходить під жодну з умов');
+//     }
+// }
+
+//Вправа 3
+
+// let checkingTypes = [ //створюємо масив в змінній 
+//     1,
+//     'STRING',
+//     true,
+//     null
+// ]
+// console.log(checkingTypes); ..виводимо масив в консоль
+// checkingTypes.push(prompt('Please, text anything in here: ', '')); виводимо в консоль масив з новим елементом, який вводить користувач через діалогове prompt
+// console.log(checkingTypes[4]);
+// checkingTypes.shift(); //видаляємо перший елемент масиву
+// console.log(checkingTypes)
+
+//Вправа 4
+
+// let cities = ["Rome", "Lviv", "Warsaw"]; 
+// console.log((cities[0] + '*' + cities[1] + '*' + cities[2])) //об'єднання через додавання роздільника як рядка і почерговий вивід масиву
+// console.log(cities.join('*')) //об'єднання через метод join, який об'єднує всі елементи масиву в один рядок і розділяє їх через роздільник (separator)
