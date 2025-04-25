640bef186860ca4d221889fe75628a9cc8056bf8
pixel <biien.deniis@gmail.com>
2025-04-25 23:20:11 +0300

    finished 2 exercises from Task 3

---
+++ ThirdHomework/script.js
@@ -0,0 +1,31 @@
+//Вправа 1
+// let numbers = [2, 3, 4, 5];
+
+// let result = 1;
+
+// for (const num of numbers) { //для всіх num в numbers виконується множення і збереження в result
+//     result *= num;
+// }
+
+// result2 = 1;
+// let i = 0;
+
+// while (i < numbers.length) { //проходить (ітерує) цикл по індексам та перемножає елементи і зберігає в результат 2
+//     result2 *= numbers[i];
+//     i++
+// }
+// console.log(result)
+// console.log(result2)
+
+//Вправа 2
+
+// let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+
+// for (num in numbers) { //для кожного елементу масиву виконується перевірка
+//     if (num % 2 !== 0) { //якщо остача не дорівнює 0 - число непарне
+//         console.log(num + '- непарне число')
+//     } else { //інакше - парне
+//         console.log(num + '- парне число')
+//     }
+// }
