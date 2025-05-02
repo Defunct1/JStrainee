5ebb5149269c9179ed51a284823e0c60c7748913
pixel <biien.deniis@gmail.com>
2025-05-02 22:46:50 +0300

    finished 4 task and complete last 2 exercises

--- FourthHomework/script.js
+++ FourthHomework/script.js
@@ -47,3 +47,59 @@
 // }
 
 // checkAge();
+
+//вправа 3
+
+// class MonthException {
+//     constructor(message) {
+//         this.message = message;
+//         this.name = 'MonthException';
+//     }
+// }
+
+// function showMonthName(month) {
+//     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//     //перевірка, чи month знаходиться в межах від 1 до 12 і якщо ні - викидає помилку Incorrect month number
+//     if(month < 1 || month > 12) {
+//         throw new MonthException('Incorrect month number');
+//     }
+//     //якщо в межах діапазону - повертає назву місяця з масиву - 1 (на 1 менше, бо індекси рахуються з 0)
+//     return months[month - 1];
+
+// }
+// try {
+//     console.log(showMonthName(5));
+//     console.log(showMonthName(15));
+
+// } catch (e) {
+//     console.log(`${e.name} ${e.message}`);
+// }
+
+// вправа 4 
+
+// 
+// function showUser(id) {
+//     if (id < 0) {
+//         throw new Error(`Error: ID must not be negative: ${id}`);
+//     }
+//     return { id };    
+// }
+
+// function showUsers(ids) {
+//     let result = [];
+//     for (let id of ids) {
+        
+//         try  {
+//             // Зберігаємо результат
+//             let user = showUser(id);
+//             // Додаємо до масиву User
+//             result.push(user);
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+        
+//     }
+//     // Повертаємо результат після всіх ітерацій
+//     return result; 
+// }
+// console.log(showUsers([85,34,123,-12]))
