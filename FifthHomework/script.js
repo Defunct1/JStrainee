8443f1ebe9d3adc4f6efd35b525e10a059922c6a
pixel <biien.deniis@gmail.com>
2025-05-03 09:26:40 +0300

    finished 2 exercises

---
+++ FifthHomework/script.js
@@ -0,0 +1,30 @@
+// Вправа 1
+
+// function propsCount(currentObject) {
+//     let keys = Object.keys(currentObject); //за допомогою метода keys отримуємо ключі об'єкту і зберігаємо їх в keys
+//     return keys.length; //повертаємо довжину масиву keys
+// }
+
+
+// let mentor = { age: 12, job:'teacher', duration: 3, unit: 'soldier' }
+// console.log(propsCount(mentor))
+
+// Вправа 2
+
+// const typesOfData = { //створюємо об'єкт з 5 елементів
+//     str : 'String',
+//     num : Number,
+//     boo : Boolean,
+//     nul: null,
+//     notANumber : NaN,
+
+// }
+
+// function showProps(obj) { //створює масив ключів за допомогою Object.keys(obj)
+//     let keys = Object.keys(obj); //масив ключів
+//     let values = Object.values(obj); //масив значень
+//     console.log('Keys:', keys); //виводить масив keys
+//     console.log('Values:', values); //виводить масив values
+// }
+
+// showProps(typesOfData);
